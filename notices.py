15d9cc3a5e8878e8f22cefe49e8dc85afc7f2d24
prosarkerrabi <project.sarkerrabi@gmail.com>
2017-01-19 03:48:02 +0600

    add code for finding notices from BRACU

--- notices.py
+++ notices.py
@@ -23,9 +23,25 @@ def aiub_top10_notice():
             print(str(cnt)+' '+link.text)
             print('http://www.aiub.edu'+link.get('href'))
             cnt += 1
-            if (cnt >= 11):
-                break
-print('North South University')
+        if (cnt >= 11):
+            break
+def bracu_top10_notice():
+    url ='http://www.bracu.ac.bd/news-archive?field_news_department_tid_selective=46'
+    source_code=urllib.request.urlopen(url)
+    soup = BeautifulSoup(source_code.read(),"html.parser")
+    count =1
+    for linkdiv in soup.find_all('div',{'class':'field-content title'}):
+        for link in linkdiv.find_all('a'):
+            print(str(count)+' '+link.text)
+            print("http://www.bracu.ac.bd"+link.get('href'))
+            count+=1
+        if (count >= 11):
+            break
+
+
+print('\nNorth South University')
 nsu_top10_notice()
 print('\nAIUB')
 aiub_top10_notice()
+print('\nBRACU')
+bracu_top10_notice()
